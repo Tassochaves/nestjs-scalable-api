e6a0aa0e0e98d5cf13f9dbfc73fb635c1ff16618
Tassochaves <tassogt@hotmail.com>
2025-05-08 23:19:02 -0300

    Feat: Install Prisma as ORM

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,15 +1,15 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from 'node_modules/@nestjs/common/index';
 
 @Controller('products')
 export class ProductsController {
