13043d7583dada52501abdbbe081312492b09272
Tassochaves <tassogt@hotmail.com>
2025-06-18 10:56:30 -0300

    Feat: validate product fields

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,25 @@
+import { IsNotEmpty, IsNumber, IsString, Matches, MaxLength, Min } from "class-validator";
+
 export class CreateProductDto {
+  
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @Matches(/^[a-z0-9-]+$/,{
+    message: 'Slug can only contain lowercase letters, numbers and dashes'
+  })
+  @IsNotEmpty()
   slug: string;
+
+  @MaxLength(500)
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
+  @Min(1)
+  @IsNumber({ maxDecimalPlaces: 2 })
   price: number;
 }
